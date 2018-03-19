9084250a55599eb7c55cece689bf180666d6a89e
weng271190436 <wengweiwei2006@gmail.com>
2018-03-19 09:03:27 -0500

    completed step 2

---
+++ calibrate.c
@@ -0,0 +1,27 @@
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/ktime.h>
+#include <linux/hrtimer.h>
+#include <linux/sched.h>
+#include <linux/kthread.h>
+
+static char* mode = "calibrate";
+module_param(mode, charp, 0644);
+
+static int calibrate_init(void){
+	printk(KERN_INFO "Mode is %s\n", mode);
+	return 0;
+}
+
+static void calibrate_exit(void){
+	printk(KERN_INFO "Calibrate exits.\n");
+}
+
+module_init(calibrate_init);
+module_exit(calibrate_exit);
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Diqiu Zhou, Zimu Wang, Wei Weng");
+MODULE_DESCRIPTION("Calibrate and run subtasks.");
+MODULE_VERSION("1.0");
