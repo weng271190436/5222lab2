7a6421bfd6ad9c95ef5ee632c55f4284eb07e9fa
Zimu Wang <zimuwang@Zimus-MacBook-Pro.local>
2018-03-22 19:45:16 -0500

    Add functions in module

--- calibrate.c
+++ calibrate.c
@@ -11,6 +11,24 @@
 static char* mode = "calibrate";
 module_param(mode, charp, 0644);
 
+// busy looping in the subtask
+void subtask_work(struct subtask* task) {
+	int i;
+	for (i = 0; i < task->loop_iterations_count; i++) {
+		ktime_get();
+	}
+}
+
+// TODO: parameter: list of subtasks on the same core
+void calibrate_core(void) {
+	// do nothing
+}
+
+struct subtask * subtask_lookup(struct hrtimer* timer) {
+	return container_of(timer, struct subtask, timer);
+}
+
+
 static int calibrate_init(void){
 	printk(KERN_INFO "Mode is %s\n", mode);
 	return 0;
