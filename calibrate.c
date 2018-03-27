7ae07d7ba849a9ee413f8adaa2058cc3ad9a5a3e
Zimu Wang <zimuwang@zimus-mbp.dhcp.wustl.edu>
2018-03-26 20:58:37 -0500

    a

--- calibrate.c
+++ calibrate.c
@@ -278,14 +278,14 @@ static int run_thread(void * data) {
 	cur_subtask->timer->function = &timer_expire;
 	printk(KERN_DEBUG "Running task: %s, with position %d\n", cur_subtask->name, cur_subtask->pos_in_task);
 	while (!kthread_should_stop()) {
-		printk(KERN_DEBUG "Begin work for task %s\n", cur_subtask->name);
+		//printk(KERN_DEBUG "Begin work for task %s\n", cur_subtask->name);
 		set_current_state(TASK_INTERRUPTIBLE);
 		// schedule();
-		printk(KERN_DEBUG "Resume work for task %s\n", cur_subtask->name);
+		//printk(KERN_DEBUG "Resume work for task %s\n", cur_subtask->name);
 		if (kthread_should_stop()) {
 			break;
 		}
-		printk(KERN_DEBUG "Continue work for task %s\n", cur_subtask->name);
+		//printk(KERN_DEBUG "Continue work for task %s\n", cur_subtask->name);
 		cur_subtask->last_release_time = ktime_get();
 
 		subtask_work(cur_subtask);
@@ -295,12 +295,12 @@ static int run_thread(void * data) {
 		period = ktime_set(0, parent_task->period);
 		// schedule next wakeup
 		if (cur_subtask->pos_in_task == 0) {
-			printk(KERN_DEBUG "Task %s is first, scheduling next\n", cur_subtask->name);
+			//printk(KERN_DEBUG "Task %s is first, scheduling next\n", cur_subtask->name);
 			hrtimer_forward(cur_subtask->timer, cur_subtask->last_release_time, period);
 		}
 		// schedule next subtask
 		if ((cur_subtask->pos_in_task != parent_task->subtask_count - 1)) {
-			printk(KERN_DEBUG "Task %s not last, schedule next subtask\n", cur_subtask->name);
+			//printk(KERN_DEBUG "Task %s not last, schedule next subtask\n", cur_subtask->name);
 			struct subtask next_subtask = parent_task->subtasks[cur_subtask->pos_in_task + 1];
 			ktime_t cur_time = ktime_get();
 			ktime_t next_wakeup = ktime_add(next_subtask.last_release_time, period);
